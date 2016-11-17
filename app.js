5b25d6b13c4dd5cf73e82104a168f7a0ebac1de6
Josh Owen <joshalldaylong@gmail.com>
2016-11-17 18:31:43 -0500

    First commit, html & css mostly

---
+++ app.js
@@ -0,0 +1,8 @@
+var myMove = function() {
+  console.log('what the what?');
+}
+
+function myFunction() {
+    document.getElementById("1").addEventListener ("click", checkIfHit);
+  console.log('broken');
+}
