00129333ce06135cd800751369cf79f080e6b105
Josh Owen <joshalldaylong@gmail.com>
2016-11-21 16:44:01 -0500

    added game feedback and reset button

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+
 var shipsFound = 0;
 var guess;
 var box;
 
 //var ship1 = #1;
 var myMove = function() {
-  console.log('what the what?');
+
 }
 while (ship1 == ship2 || ship2 == ship3 || ship1 == ship3) {
   // Using three variables to save our ship positions
@@ -15,6 +16,149 @@ while (ship1 == ship2 || ship2 == ship3 || ship1 == ship3) {
   var ship2 = Math.floor(Math.random() * 26);
   var ship3 = Math.floor(Math.random() * 26);
 }
+var boxes = document.getElementsByClassName("space");
+
+for(var i = 0; i < boxes.length; i++) {
+  // Declare variable to hold each single box
+  var box = boxes[i];
+  // Creating an onclick event on the variabled we just declared
+ box.onclick = function() {
+    var str = this.getAttribute('id');
+    var position = parseInt(str);
+    torpedoes();
+
+
+          //alert("You Win!");
+    if (torpedostack < 0) {
+      $( "body" ).off( "click", ".space", boxes);
+    }
+    else if (shipsFound === 3) {
+      $( "body" ).off( "click", ".space", boxes);
+    }
+    else if (torpedostack === 0 && shipsFound === 3) {
+      $("#ammo").html("You Win!");
+    }
+    else if (position == ship1 || position == ship2 || position == ship3) {
+        // If we get a hit, the box changes to black
+        this.style.color = "red";
+        shipsFound++;
+        if (shipsFound === 3) {
+              $("#gameFeedback").html("You Win!");
+        } else {
+          $("#gameFeedback").html("That's a hit!");
+        };
+    }
+    else {
+        this.style.backgroundColor = "grey";
+        $("#gameFeedback").html("Miss");
+
+  }
+}
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+    }
+}
+
+          // 1. Need to make sure Onclick function for torpedoes() when tile is clicked works
+          // 2. Add Jquery selector function on click to change html of #ammo
+
+
+
+//var ammoRemaining= (16 - guess);
+//var ammoRemaining = 16;
+//ammoRemaining--;
+//var z = ammoRemaining;
+//document.getElementById("demo").innerHTML = z;
+
+var torpedostack = 25;
+
+function torpedoes() {
+  console.log("Torpedo fired");
+  --torpedostack;
+  if (torpedostack >= 0) {
+    $("#ammo").html("You have " + torpedostack + " torpedoes remaining");
+  }
+  else if (shipsFound === 3) {
+    $( "body" ).off( "click", ".space", boxes);
+  }
+  else if (torpedostack === 0 && shipsFound === 3) {
+    $("#ammo").html("You Win!");
+  }
+  else {
+    $("#ammo").html("Game Over");
+    //alert("Game Over");
+
+  }
+}
+
+// function hitOrMiss() {
+//  if ()
+//  }
+/*
+    var shipsFound = 0;
+    var guess;
+    var box;
+    var torpedostack = 16;
+    var myMove = function() {}
+
+
+    var model = {
+        boardSize: 5,
+        numShips: 3,
+        shipLength: [4, 3, 2],
+        shipsSunk: 0,
+
+    	ship1: [
+    		{ locations: [0, 0, 0, 0], hits: ["", "", "", ""] },
+    	],
+      ship2: [
+        { locations: [0, 0, 0], hits: ["", "", ""] },
+      ],
+      ship3: [
+        { locations: [0, 0], hits: ["", ""] }
+      ],
+
+      generateShipLocations: function() {
+		var locations;
+		for (var i = 0; i < this.numShips; i++) {
+		do {
+				locations = this.generateShip();
+		} while (this.collision(locations));
+			this.ships[i].locations = locations;
+		}
+	},
+  generateShip: function() {
+  var direction = Math.floor(Math.random() * 2);
+  var row, col;
+
+  if (direction === 1) { // horizontal
+    row = Math.floor(Math.random() * this.boardSize);
+    col = Math.floor(Math.random() * (this.boardSize - this.shipLength + 1));
+  } else { // vertical
+    row = Math.floor(Math.random() * (this.boardSize - this.shipLength + 1));
+    col = Math.floor(Math.random() * this.boardSize);
+  }
+
+  var newShipLocations = [];
+
+  for (var i = 0; i < this.shipLength; i++) {
+    if (direction === 1) {
+      newShipLocations.push(row + "" + (col + i));
+    } else {
+      newShipLocations.push((row + i) + "" + col);
+    }
+  }
+  return newShipLocations;
+}
+
+
 var boxes = document.getElementsByClassName("space");
 
 for(var i = 0; i < boxes.length; i++) {
@@ -42,17 +186,7 @@ for(var i = 0; i < boxes.length; i++) {
   }
 }
 }
-
-          // 1. Need to make sure Onclick function for torpedoes() when tile is clicked works
-          // 2. Add Jquery selector function on click to change html of #ammo
-
-
-
-//var ammoRemaining= (16 - guess);
-/*var ammoRemaining = 16;
-ammoRemaining--;
-var z = ammoRemaining;
-document.getElementById("demo").innerHTML = z;*/
+};
 
 var torpedostack = 16;
 
@@ -66,51 +200,4 @@ function torpedoes() {
     alert("Game over");
   }
 }
-    // document.getElementById("ammo").innerHTML = "You have 15 torpedoes remaining";
-
-
-
-  //
-  // what the fuck is happening below this point?
-  function addClassByClick(){
-    $(id"").addClass('disabled');
-   }
-
-
-
-/*
-var game = function(){
-
-function gridGuess(guess) {
-  if (guess[i] === ship[i]) {
-    alert('That\'s A Hit!')
-  }
-  else {
-    alert('Miss')
-  }
-}
-
-showWinner: function() {
-},
-
-showGameOver: function() {
-},
-
-var reset = function() {},
-
-var move = function(r, c) {
-    this.board[r][c] = this.currentPlayer;
-  },
-
-var myMove = function() {
-  console.log('what the what?');
-},
-
-function myFunction() {
-    document.getElementById("1").addEventListener ("click", checkIfHit);
-},
-
-var ammo = function(guess) {
-  (16 - guess);
-
-} */
+*/
